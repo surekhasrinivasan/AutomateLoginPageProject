d59dc73a5959e386839c7b40674153103227599f
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-15 11:29:15 -0400

    Web browser opens yahoo.com

--- AutomateLoginPageProject/TestBase.cs
+++ AutomateLoginPageProject/TestBase.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,28 @@ namespace AutomateLoginPageProject
 {
     public class TestBase
     {
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(); //launches chrome browser
+            driver.Manage().Window.Maximize();//maximizes the window
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);//set LocatorTimeout to be 20 seconds at max
+        }
+        
+        [Test]
+        public void OpenWebPage()
+        {
+            driver.Url = "https://www.yahoo.com/";
+            driver.FindElement(By.XPath("//*[@id=\"uh-signin\"]")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        } 
+        
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
     }
 }
