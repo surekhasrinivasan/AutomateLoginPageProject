102724b48722373e975cd686f3846bc0b2e3ea5f
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-16 11:20:35 -0400

    LoginTests.cs now has 2 tests for verify and validating username and password

--- AutomateLoginPageProject/LoginTests.cs
+++ AutomateLoginPageProject/LoginTests.cs
@@ -35,7 +35,22 @@ namespace AutomateLoginPageProject
         [Test]
         public void VerifyInvalidLogin()
         {
-            
+            //Open the login page
+            driver.Url = "https://www.yahoo.com/";
+            driver.FindElement(By.XPath("//*[@id=\"uh-signin\"]")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Console.WriteLine("Opening " + driver.Url);
+
+            //Enter the username and password
+            driver.FindElement(By.XPath("//*[@id=\"login-username\"]")).SendKeys("surekha.srinivasan@yahoo.com");
+            driver.FindElement(By.Id("login-signin")).Click();
+
+            driver.FindElement(By.Id("login-passwd")).SendKeys("test");
+            Console.WriteLine("Enter username and password");
+
+            //Click Signin button
+            driver.FindElement(By.Name("verifyPassword")).Click();
+            Console.WriteLine("Clicked Login Button");
         }
     }
 }
