d86ae302c577d4755ee2ea1d692391af2a47bd74
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-04 22:39:08 -0400

    SeleniumGoogleLoginProject - initial commit

---
+++ SeleniumGoogleLoginProject/SeleniumGoogleLoginDemo/SeleniumGoogleLoginDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGoogleLoginDemo
+{
+    // Program does the following:
+    // 
+    // Launch the browser and open “Gmail.com”.
+    // Verify the title of the page and print the verification result.
+    // Enter the username and Password.
+    // Click on the Sign in button.
+    // Close the web browser.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
